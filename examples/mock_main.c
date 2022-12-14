f3f3936a0bd96c847398e04ada6fd41fd039d498
michael <korres2002@gmail.com>
2022-12-14 21:07:16 +0200

    Looks like it works perfectly fine.Added some code in the mock_main and the result seems to be the wanted one.

--- examples/mock_main.c
+++ examples/mock_main.c
@@ -24,9 +24,41 @@ int main() {
   HP_info* curr_HP_info = HP_OpenFile(FILE_NAME);
 
 
-  HP_info_Print(curr_HP_info);
+  // HP_info_Print(curr_HP_info);
+
+  Record rec = randomRecord();
+
+  HP_InsertEntry(curr_HP_info,rec);
+
+  /////////////////////////
+
+  //CALL_OR_DIE(BF_Init(LRU));
+  BF_Block *block;
+  BF_Block_Init(&block);
+
+  
+  
+  int blocks_num;
+  int fd1 = curr_HP_info->file_describer;
+  CALL_OR_DIE(BF_GetBlockCounter(fd1, &blocks_num));
+
+  void* data;
+
+  for (int i = 1; i < blocks_num; ++i) {
+    printf("Contents of Block %d\n\t",i);
+    CALL_OR_DIE(BF_GetBlock(fd1, i, block));
+    data = BF_Block_GetData(block);
+    Record* rec= data;
+    printRecord(rec[0]);
+    // printf("\t");
+    // printRecord(rec[1]);
+    CALL_OR_DIE(BF_UnpinBlock(block));
+  }
+
+  BF_Block_Destroy(&block);
 
 
+  //////////////////////
 
   HP_CloseFile(curr_HP_info);
   BF_Close();
